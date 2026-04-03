0fa89bbd1a7ba8cfd110c5dc40c24fdc280dc690
Guilherme Lins <guilherme.lins@fatec.sp.gov.br>
2026-04-03 19:17:02 -0300

    Adição do método de upload de modelos

--- backend/src/main/java/com/deeremail/controllers/templatesController.java
+++ backend/src/main/java/com/deeremail/controllers/templatesController.java
@@ -1,11 +1,16 @@
 package com.deeremail.controllers;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.deeremail.DTOs.FileNode;
 import com.deeremail.utils.FileStructure;
@@ -15,11 +20,48 @@ import com.deeremail.utils.FileStructure;
 @RequestMapping("/api/templates")
 public class templatesController {
 
+    // Caminho fixo por enquanto, alterar durante o desenvolvimento
+    private String templatePath = "/home/guilherme/Documents/PI_2026_1/dev/mailTemplates";
+
     @GetMapping("/list")
     //lista as pastas e os arquivos existentes dentro em uma estrutura de json com a hierarquia
     public FileNode getFiles() throws IOException {
-        // Caminho fixo por enquanto, alterar ddurante o desenvolvimento
-        String fixedPath = "mailTemplates";
-        return FileStructure.getFolderStructure(fixedPath);
+        return FileStructure.getFolderStructure(templatePath);
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+        @RequestParam MultipartFile file,
+        @RequestParam String name,
+        @RequestParam String version
+    ) {
+
+        String fullPath = templatePath + "/" + name + "/" + version;
+
+        // Retorna erro se a chamada da api vier sem o arquivo
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("Arquivo está vazio");
+        }
+
+        try {
+            // Cria a pasta de template se ela não existir
+            File directory = new File(fullPath);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            // Salva o arquivo
+            String filePath = fullPath + "/" + file.getOriginalFilename();
+            File dest = new File(filePath);
+            file.transferTo(dest);
+
+            return ResponseEntity.ok("File uploaded to: " + filePath);
+
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body("Erro no upload");
+        }
+    }
+
+
+
 }

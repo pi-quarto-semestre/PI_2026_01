4cc0483f2294fe1a80495081ad8f1170ccaf23f0
Pedro Tozaki <pedro.tozaki@fatec.sp.gov.br>
2026-04-08 23:40:58 -0300

    Ajuste para funcionamento do Flyway

--- backend/src/main/java/com/deeremail/mailscheduler/MailschedulerApplication.java
+++ backend/src/main/java/com/deeremail/mailscheduler/MailschedulerApplication.java
@@ -11,11 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 import com.deeremail.utils.Config;
 
 @PropertySource("file:settings.ini")
-
-@ComponentScan("com.deeremail.controllers")
-@ComponentScan("com.deeremail.DTOs")
-@ComponentScan("com.deeremail.utils")
-
+@ComponentScan("com.deeremail")
 @SpringBootApplication
 public class MailschedulerApplication {
 

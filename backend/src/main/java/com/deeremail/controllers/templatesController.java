0736c113a5393751737ceb61ee228df538c2d111
Guilherme Lins <guilherme.lins@fatec.sp.gov.br>
2026-04-12 17:40:09 -0300

    Adicionando API - Retorno de detalhes de um template

--- backend/src/main/java/com/deeremail/controllers/templatesController.java
+++ backend/src/main/java/com/deeremail/controllers/templatesController.java
@@ -2,8 +2,17 @@ package com.deeremail.controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +37,33 @@ public class templatesController {
     // lista as pastas e os arquivos existentes dentro em uma estrutura de json com a hierarquia
     public FileNode getFiles() throws IOException {
         return FileStructure.getFolderStructure(templatePath);
+    }
+
+    @GetMapping("/template")
+    // Retorna o modelo da versão especificada como texto e seus parâmetros como um array
+    public Map<String, Object> getTemplate() throws IOException {
+        
+        // Monta o caminho da pasta para o modelo html
+        ClassPathResource resource = new ClassPathResource("/home/guilherme/Documents/PI_2026_1/dev/mailTemplates/teste/v1/template.html");
+        String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+
+        // Encontra todos os parâmetros no formato ${parametro}
+        Pattern pattern = Pattern.compile("\\$\\{([^}]+)}");
+        Matcher matcher = pattern.matcher(content);
+
+        // Cria a lista de parâmetros
+        Set<String> params = new HashSet<>();
+
+        // Percorre os parâmetros encontrados e adiciona eles na lista
+        while (matcher.find()) {
+            params.add(matcher.group(1));
+        }
+
+        // Cria a resposta e retorna
+        Map<String, Object> response = new HashMap<>();
+        response.put("content", content);
+        response.put("parameters", params);
+        return response;
     }
 
     @PostMapping("/upload")

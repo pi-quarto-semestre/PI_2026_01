77b2f9f9e29a97696d73480c358639b5bbab4475
Guilherme Lins <guilherme.lins@fatec.sp.gov.br>
2026-04-14 21:38:25 -0300

    Revert "Banco local como padrão, Supabase opcional via perfil cloud, novas migrations de categorias/tags e"
    This reverts commit bbe85c9dfdc71c99cf85ed3c382d8fecc9a403f0.

--- backend/src/main/java/com/deeremail/mailscheduler/MailschedulerApplication.java
+++ backend/src/main/java/com/deeremail/mailscheduler/MailschedulerApplication.java
@@ -1,9 +1,14 @@
 package com.deeremail.mailscheduler;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+
+import com.deeremail.utils.Config;
 
 @PropertySource("file:settings.ini")
 @ComponentScan("com.deeremail")
@@ -11,6 +16,14 @@ import org.springframework.context.annotation.PropertySource;
 public class MailschedulerApplication {
 
 	public static void main(String[] args) {
+		try {
+			Config.getConfig();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		SpringApplication.run(MailschedulerApplication.class, args);
 	}
 

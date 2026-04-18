108f958d0b8bc05f57bf51341e2f562635c40565
Guilherme Lins <guilherme.lins@fatec.sp.gov.br>
2026-04-17 22:19:26 -0300

    Adaptando API para receber parâmetros

--- backend/src/main/java/com/deeremail/controllers/mailController.java
+++ backend/src/main/java/com/deeremail/controllers/mailController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,16 +26,22 @@ public class mailController {
 
     @PostMapping("/sendNow")
     // Envia o email imediatamente
-    public ResponseEntity<String> sendNow() throws IOException {
+    public ResponseEntity<String> sendNow(
+        @RequestParam String name,
+        @RequestParam String version,
+        @RequestParam String subject,
+        @RequestParam String sendTo,
+        @RequestParam String sendCcTo[]
+    ) throws IOException {
 
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper; 
         
         try {
             helper = new MimeMessageHelper(message, true);
-            helper.setTo("teste@gmail.com");
+            helper.setTo(sendTo);
             //helper.setCc("copy@example.com"); // [11]
-            helper.setSubject("Teste de email");
+            helper.setSubject(subject);
             helper.setText("<h1>Teste 123</h1>", true);
 
         } catch (MessagingException e) {

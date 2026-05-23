3038234be82b3eb04b6826ce51e5ebde6933cbff
Guilherme Lins <guilherme.lins@fatec.sp.gov.br>
2026-05-23 15:47:32 -0300

    Removendo imports não usados

--- backend/src/main/java/com/deeremail/infra/SecurityConfiguration.java
+++ backend/src/main/java/com/deeremail/infra/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.deeremail.infra;
 
-import com.deeremail.DTOs.user.User;
 import com.deeremail.services.AuthorizationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
